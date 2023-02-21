de44851f928261e8160a73010d71f1b4a37e4099
Emmanuel Osuolale <yansmonies@gmail.com>
2023-02-21 12:29:39 -0800

    up

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,8 +7,11 @@ int main(void)
 {
 	/* your code goes there */
 	for ( i=0; i <26;  i++) {
-	  putchar('a' + i)
-	}
+	  if (i == 4 or i == 16) {
+	   pass 
+	  } else {
+	      	putchar('a' + i)
+}} 
 	putchar('\n')
 	return (0);
 }
