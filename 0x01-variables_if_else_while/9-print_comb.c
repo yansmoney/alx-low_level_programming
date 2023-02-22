6b1bd1337d0aa88d45390cfd6f696ddb77822793
Emmanuel Osuolale <yansmonies@gmail.com>
2023-02-22 13:19:48 -0800

    fixed another issue

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,16 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 
 	}
 	putchar('\n');
-
 	return (0);
 }
