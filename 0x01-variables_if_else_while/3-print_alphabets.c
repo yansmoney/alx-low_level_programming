fe9cea246b4bfe88d9aab8247122a31e6389115c
Emmanuel Osuolale <yansmonies@gmail.com>
2023-02-21 22:15:07 -0800

    ss

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,17 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
+	int i;
 	/* your code goes there */
-	for (int i=0; i <26;  i++) {
+	for (i=0; i <26;  i++) {
 	  putchar('a' + i);
 	}
-	for (int i=0; i <26;  i++) {
+	for (i=0; i <26;  i++) {
           putchar('A' + i);
         }
 	putchar('\n');
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,17 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
+	int i;
 	/* your code goes there */
 	for ( i=0; i <26;  i++) {
-	  if (i == 4 or i == 16) {
-	   pass 
-	  } else {
-	      	putchar('a' + i)
+	  if (i != 4 or i != 16) {
+	      	putchar('a' + i);
 }} 
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
